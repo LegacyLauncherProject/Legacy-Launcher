c86e54ebc85b54c12a1ce352e2b5689763f29370
LukesicoolDEV <lukeiscool2543@gmail.com>
2026-06-14 21:16:23 +0800

    Revert package name

--- ZalithLauncher/build.gradle.kts
+++ ZalithLauncher/build.gradle.kts
@@ -27,7 +27,7 @@ val getBuildType = {
     buildType
 }
 
-val nameId = "com.lukeiscooldev.legacylaunch"
+val nameId = "com.movtery.zalithlauncher"
 val generatedZalithDir = file("$buildDir/generated/source/zalith/java")
 val launcherAPPName = project.findProperty("launcher_app_name") as? String ?: error("The \"launcher_app_name\" property is not set in gradle.properties.")
 val launcherName = project.findProperty("launcher_name") as? String ?: error("The \"launcher_name\" property is not set in gradle.properties.")
